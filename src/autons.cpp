b5d7363d123f3991abed790b4c6237a18439bd4d
Sahil Patel <srpthegoat@gmail.com>
2025-10-25 10:44:25 -0400

    v

--- src/autons.cpp
+++ src/autons.cpp
@@ -56,7 +56,7 @@ void low_goal_auto() {
   chassis.pid_wait();
   chassis.pid_turn_set(46_deg, 70);
   chassis.pid_wait();
-  chassis.pid_odom_set({{17_in, 28_in}, fwd, 30}, true);
+  chassis.pid_odom_set({{16.6_in, 28_in}, fwd, 30}, true);
   chassis.pid_wait();
   pros::delay(1000);
   chassis.pid_odom_set({{8.5_in, 33_in, -43_deg}, fwd, 70}, true);
@@ -70,11 +70,10 @@ void low_goal_auto() {
   scraper.set(true); // Deploy scraper
   basket();
   pros::delay(500);
-  chassis.pid_odom_set({{40.5_in, -11.5_in, 180_deg}, fwd, 120}, true);
+  chassis.pid_odom_set({{40.5_in, -12.75_in, 180_deg}, fwd, 127}, true);
   chassis.pid_wait();
   basket();
-  pros::delay(3000); 
-  chassis.pid_wait();
+  pros::delay(1500); 
   chassis.pid_odom_set({{40.5_in, 0_in, 180_deg}, rev, 110}, true);
   chassis.pid_wait();
   scraper.set(false); // Retract scraper
@@ -90,21 +89,28 @@ void middle_goal_auto() {
   chassis.pid_wait();
   chassis.pid_turn_set(-46_deg, 70);
   chassis.pid_wait();
-  chassis.pid_odom_set({{-17_in, -28_in}, fwd, 30}, true);
+  chassis.pid_odom_set({{-17_in, 28.4_in}, fwd, 30}, true);
   chassis.pid_wait();
   pros::delay(1000);
-  chassis.pid_odom_set({{-8.5_in, -33_in, 45_deg}, fwd, 70}, true);
+  chassis.pid_odom_set({{-8.5_in, 33_in, 43_deg}, fwd, 70}, true);
   chassis.pid_wait();
   middle_goal();
   pros::delay(2000);
-  chassis.pid_odom_set({{-41_in, -10_in}, rev, 110}, true);
+  chassis.pid_odom_set({{-40.5_in, 10_in}, rev, 110}, true);
   chassis.pid_wait(); 
   chassis.pid_turn_set(180_deg, 50);
   chassis.pid_wait();// Ensure the drive command completes before ending the function
   scraper.set(true); // Deploy scraper
   basket();
   pros::delay(500);
-  chassis.pid_odom_set({{-41_in, 7_in, 180_deg}, fwd, 110}, true);
+  chassis.pid_odom_set({{-40.5_in, -13_in, 180_deg}, fwd, 127}, true);
+  chassis.pid_wait();
+  basket();
+  pros::delay(1500); 
+  chassis.pid_odom_set({{-40.5_in, 0_in, 180_deg}, rev, 110}, true);
+  chassis.pid_wait();
+  scraper.set(false); // Retract scraper
+  chassis.pid_turn_set(0_deg, 100);
   chassis.pid_wait();
  }
 ///
--- src/main.cpp
+++ src/main.cpp
@@ -46,7 +46,7 @@ void initialize() {
 
   // Configure your chassis controls
   chassis.opcontrol_curve_buttons_toggle(true);   // Enables modifying the controller curve with buttons on the joysticks
-  chassis.opcontrol_drive_activebrake_set(0.0);   // Sets the active brake kP. We recommend ~2.  0 will disable.
+  chassis.opcontrol_drive_activebrake_set(2.0);   // Sets the active brake kP. We recommend ~2.  0 will disable.
   chassis.opcontrol_curve_default_set(0.0, 0.0);  // Defaults for curve. If using tank, only the first parameter is used. (Comment this line out if you have an SD card!)
 
   // Set the drive to your own constants from autons.cpp!
@@ -60,20 +60,20 @@ void initialize() {
   ez::as::auton_selector.autons_add({
       {"Score in Low Goal from Mid Line", low_goal_auto},
       {"Score in Middle Goal from Mid Line", middle_goal_auto},
-      //{"Drive\n\nDrive forward and come back", drive_example},
-      //{"Turn\n\nTurn 3 times.", turn_example},
-      //{"Drive and Turn\n\nDrive forward, turn, come back", drive_and_turn},
-      //{"Drive and Turn\n\nSlow down during drive", wait_until_change_speed},
-      //{"Swing Turn\n\nSwing in an 'S' curve", swing_example},
-      //{"Motion Chaining\n\nDrive forward, turn, and come back, but blend everything together :D", motion_chaining},
-      //{"Combine all 3 movements", combining_movements},
-      //{"Interference\n\nAfter driving forward, robot performs differently if interfered or not", interfered_example},
-      //{"Simple Odom\n\nThis is the same as the drive example, but it uses odom instead!", odom_drive_example},
-      //{"Pure Pursuit\n\nGo to (0, 30) and pass through (6, 10) on the way.  Come back to (0, 0)", odom_pure_pursuit_example},
-      //{"Pure Pursuit Wait Until\n\nGo to (24, 24) but start running an intake once the robot passes (12, 24)", odom_pure_pursuit_wait_until_example},
-      //{"Boomerang\n\nGo to (0, 24, 45) then come back to (0, 0, 0)", odom_boomerang_example},
-      //{"Boomerang Pure Pursuit\n\nGo to (0, 24, 45) on the way to (24, 24) then come back to (0, 0, 0)", odom_boomerang_injected_pure_pursuit_example},
-      //{"Measure Offsets\n\nThis will turn the robot a bunch of times and calculate your offsets for your tracking wheels.", measure_offsets},
+      {"Drive\n\nDrive forward and come back", drive_example},
+      {"Turn\n\nTurn 3 times.", turn_example},
+      {"Drive and Turn\n\nDrive forward, turn, come back", drive_and_turn},
+      {"Drive and Turn\n\nSlow down during drive", wait_until_change_speed},
+      {"Swing Turn\n\nSwing in an 'S' curve", swing_example},
+      {"Motion Chaining\n\nDrive forward, turn, and come back, but blend everything together :D", motion_chaining},
+      {"Combine all 3 movements", combining_movements},
+      {"Interference\n\nAfter driving forward, robot performs differently if interfered or not", interfered_example},
+      {"Simple Odom\n\nThis is the same as the drive example, but it uses odom instead!", odom_drive_example},
+      {"Pure Pursuit\n\nGo to (0, 30) and pass through (6, 10) on the way.  Come back to (0, 0)", odom_pure_pursuit_example},
+      {"Pure Pursuit Wait Until\n\nGo to (24, 24) but start running an intake once the robot passes (12, 24)", odom_pure_pursuit_wait_until_example},
+      {"Boomerang\n\nGo to (0, 24, 45) then come back to (0, 0, 0)", odom_boomerang_example},
+      {"Boomerang Pure Pursuit\n\nGo to (0, 24, 45) on the way to (24, 24) then come back to (0, 0, 0)", odom_boomerang_injected_pure_pursuit_example},
+      {"Measure Offsets\n\nThis will turn the robot a bunch of times and calculate your offsets for your tracking wheels.", measure_offsets},
   });
 
   // Initialize chassis and auton selector
@@ -269,9 +269,9 @@ void opcontrol() {
   if (master.get_digital(DIGITAL_L1)) {
     // middle goal
     piston1.set(false); 
-    front_intake.move(100);
-    back_intake.move(100);
-    top_intake.move(-100);
+    front_intake.move(80);
+    back_intake.move(80);
+    top_intake.move(-80);
   } else if (master.get_digital(DIGITAL_L2)) {
     // outtake
     piston1.set(false);
